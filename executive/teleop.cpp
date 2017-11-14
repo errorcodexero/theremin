405ff20baa489ea5147e3af22375aebbe61ba98f
Matthew Macovsky <macovsky.m@gmail.com>
2017-11-13 18:52:13 -0800

    Remove some outdated comments

--- executive/teleop.cpp
+++ executive/teleop.cpp
@@ -71,9 +71,9 @@ Toplevel::Goal Teleop::run(Run_info info) {
 			if(!nudges[Nudges::COUNTERCLOCKWISE].timer.done()) return -ROTATE_NUDGE_POWER;
 			double power=set_drive_speed(-info.driver_joystick.axis[Gamepad_axis::LEFTY],boost,slow);
 			if(spin) power+=set_drive_speed(info.driver_joystick.axis[Gamepad_axis::RIGHTX],boost,slow);
-			return power; //inverted so drivebase values can be positive
+			return power;
 		}());
-		double right=clip([&]{ //right side is reversed
+		double right=clip([&]{
 			if(!nudges[Nudges::FORWARD].timer.done()) return NUDGE_POWER;
 			if(!nudges[Nudges::BACKWARD].timer.done()) return -NUDGE_POWER;
 			if(!nudges[Nudges::CLOCKWISE].timer.done()) return -ROTATE_NUDGE_POWER;	
