fe526346ea920f907b9f767df050ba0bd89a9941
Software3 <1425sw@gmail.com>
2017-12-02 14:08:55 -0800

    Add camera lights test switch

--- executive/teleop.cpp
+++ executive/teleop.cpp
@@ -86,7 +86,8 @@ Toplevel::Goal Teleop::run(Run_info info) {
 		goals.drive = Drivebase::Goal::absolute(left,right);
 	}
 
-	if(info.driver_joystick.button[Gamepad_button::A]) goals.drive = Drivebase::Goal::rotate(270);
+	if(info.driver_joystick.button[Gamepad_button::B]) goals.lights.camera_light = Lights::Camera_light::ON;
+	else goals.lights.camera_light = Lights::Camera_light::OFF;
 
 	#ifdef PRINT_OUTS
 	if(info.in.ds_info.connected && (print_number%10)==0){
